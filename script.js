c0d37063758a88f8ca2568c8b8b96658ad2df001
Danny Castillo <dannyhgo@hotmail.com>
2021-05-07 22:05:23 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// variables
+let accordions = document.querySelectorAll('.accordion'); 
+
+// loops trough accordions and add event listener to all
+accordions.forEach(accord => accord.addEventListener('click', () => {
+    // dynamic select accordion content
+    let content = accord.nextElementSibling;
+    
+    // add or remove active class
+    accord.classList.toggle('active');
+    
+    // if accordion open close else open
+    content.style.maxHeight ? content.style.maxHeight = null : content.style.maxHeight = `${content.scrollHeight}px`;
+}));
+
+
